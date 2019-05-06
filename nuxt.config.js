6e575e4246d437a2538d69c2e9cb22330cfd8856
fanxiaodong <fanxiaodong@datafountain.cn>
2019-05-06 11:19:30 +0800

    feat: add server api with express

---
+++ nuxt.config.js
@@ -0,0 +1,35 @@
+const modules = ["@nuxtjs/axios", "~/modules/i18n-json"];
+// export default {
+module.exports = {
+    mode: 'universal',
+    srcDir: "./client",
+    env: {},
+    head: {
+        title: "name",
+        meta: [{
+                charset: "utf-8"
+            },
+            {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1"
+            },
+            {
+                hid: "description",
+                name: "description",
+                content: "description"
+            }
+        ],
+        link: [{
+            rel: "icon",
+            type: "image/x-icon",
+            href: "/favicon.ico"
+        }]
+    },
+    loading: {
+        color: "#3B8070"
+    },
+    css: ["~/assets/styles/main.css"],
+    build: {},
+    modules,
+    axios: {}
+};
